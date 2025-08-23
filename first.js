f6138f1e045d2ec8b7299909dab3b19858213d1a
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-08-23 19:33:38 +0530

    Add quiz questions and style next button for improved user interaction

---
+++ first.js
@@ -0,0 +1,20 @@
+const question = [
+    {
+        question: "which is largest animal in the world?",
+        answer: [
+            { text: "shark", correct: false },
+            { text: "Blue whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "which is the smallest continent in the world?",
+        answer: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true },
+            { text: "Antarctica", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    }
+]
