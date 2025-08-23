8f86c147b7526da2fe9b10399f405435c4b1de6b
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-08-23 20:15:37 +0530

    Add additional quiz questions for enhanced content variety

--- first.js
+++ first.js
@@ -16,5 +16,38 @@ const question = [
             { text: "Antarctica", correct: false },
             { text: "Africa", correct: false },
         ]
+    },
+    {
+        question: "which is the largest desert in the world?",
+        answer: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false },
+            { text: "Sahara", correct: false },
+            { text: "Antarctica", correct: true },
+        ]
+    },
+    {
+        question: "which is the smallest continent in the world?",
+        answer: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true },
+            { text: "Antarctica", correct: false },
+            { text: "Africa", correct: false },
+        ]
     }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("nxt-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion(){
